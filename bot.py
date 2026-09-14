503dd1181e8a2690c0a8a89a5d0b650977ac6e13
021amirho021-maker <021amirho021@gmail.com>
2026-09-14 17:51:46 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ except ImportError:
 from curl_cffi import requests as c_requests
 from rubpy import BotClient
 
-# لیست سایت‌های سازگار با سرورهای ابری
+# لیست سایت‌های فعال و سازگار
 TARGET_SITES = [
     "https://www.doostihaa.com/",
     "https://salamdl.info/"
@@ -180,7 +180,8 @@ async def main():
                     
                     try:
                         print("📤 در حال آپلود و ارسال ویدیو به همراه کپشن در کانال...")
-                        await bot.send_video(chat_id=CHAT_ID, video=trailer_file, caption=caption)
+                        # استفاده از متد استاندارد send_file در روبای برای ارسال ویدیوی پلیردار
+                        await bot.send_file(chat_id=CHAT_ID, file=trailer_file, caption=caption)
                         
                         if os.path.exists(trailer_file):
                             os.remove(trailer_file)
